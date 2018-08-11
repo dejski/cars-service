2e6b9ae5ad36e9c72ece1155c0025a5496632945
dejski <dejski@wp.pl>
2018-08-11 17:44:09 +0200

    http.get dla Angular6 - zakomentowane

--- src/app/cars/cars.service.ts
+++ src/app/cars/cars.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@angular/core'
 import { Http } from '@angular/http'
-import { Observable } from 'rxjs/Observable'
 import { Car } from './models/car'
+import { Observable } from 'rxjs/Observable'
 import 'rxjs/add/operator/map'
+// import { Observable } from 'rxjs'
+// import { map } from 'rxjs/operators'
 
 @Injectable({
   providedIn: 'root',
@@ -13,5 +15,6 @@ export class CarsService {
 
   getCars(): Observable<Car[]> {
     return this.http.get(this.apiUrl).map(res => res.json())
+    // return this.http.get(this.apiUrl).pipe(map(res => res.json()))
   }
 }
