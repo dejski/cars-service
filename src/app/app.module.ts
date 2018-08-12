73f78d22255bd7fd9da9e2f612d8beae269ee5e8
dejski <dejski@wp.pl>
2018-08-12 11:12:44 +0200

    wydzielony routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,24 @@
+import { CarsListComponent } from './cars/cars-list/cars-list.component'
 import { CarsModule } from './cars/cars.module'
 import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core'
 import { AppComponent } from './app.component'
 import { CarsService } from './cars/cars.service'
 import { HttpModule } from '@angular/http'
-import { HeaderComponent } from './shared-module/header/header.component'
 import { CoreModule } from './core-module/core-module'
+import { AppRoutingModule } from './app-routing.module'
+import { CarsRoutingModule } from './cars/cars-routings.module'
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, CarsModule, HttpModule, CoreModule],
+  imports: [
+    BrowserModule,
+    CarsModule,
+    HttpModule,
+    CoreModule,
+    AppRoutingModule,
+    CarsRoutingModule,
+  ],
   providers: [CarsService],
   bootstrap: [AppComponent],
 })
